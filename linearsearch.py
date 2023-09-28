8123ac7b6fdd8e1fb9de492a030b00b74ab87dcd
pvnmahesh999 <46401452+pvnmahesh999@users.noreply.github.com>
2023-09-28 17:59:10 -0500

    Add files via upload
    Linear Search Implementation for the possible miscellaneous test cases.

---
+++ linearsearch.py
@@ -0,0 +1,27 @@
+def linear(elements,query):
+    for i in range(0,len(elements)):
+        if elements[i]==query:
+            return i
+    return -1
+    
+testcases=[{'input': {'cards': [13, 11, 10, 7, 4, 3, 1, 0], 'query': 7}, 'output': 3},
+ {'input': {'cards': [13, 11, 10, 7, 4, 3, 1, 0], 'query': 1}, 'output': 6},
+ {'input': {'cards': [4, 2, 1, -1], 'query': 4}, 'output': 0},
+ {'input': {'cards': [3, -1, -9, -127], 'query': -127}, 'output': 3},
+ {'input': {'cards': [6], 'query': 6}, 'output': 0},
+ {'input': {'cards': [9, 7, 5, 2, -9], 'query': 4}, 'output': -1},
+ {'input': {'cards': [], 'query': 7}, 'output': -1},
+ {'input': {'cards': [8, 8, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0], 'query': 3},
+  'output': 7},
+ {'input': {'cards': [8, 8, 6, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0],
+  'query': 6},
+  'output': 2}]
+            
+for j in range(0,len(testcases)):
+    position=linear(testcases[j]['input']['cards'],testcases[j]['input']['query'])
+    if position==testcases[j]['output']:
+        print("{} test case passed".format(j+1))
+
+    
+    
+            
